2d44ecb15191d795b2207e4dba8689d43ffe239c
Swetha.Meti <swetha.meti@gmail.com>
2017-06-10 17:11:53 +0530

    Add author to blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526153735) do
+ActiveRecord::Schema.define(version: 20170610113634) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20170526153735) do
     t.datetime "published_date"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|

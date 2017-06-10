5293a8eebb7b005bc2e6619a04ac9c6967a3ee11
Swetha.Meti <swetha.meti@gmail.com>
2017-06-10 17:49:05 +0530

    subscriber

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :subscribers
   resources :advetisers
   resources :authors
   resources :categories
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610115610) do
+ActiveRecord::Schema.define(version: 20170610121638) do
 
   create_table "advetisers", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20170610115610) do
   end
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
